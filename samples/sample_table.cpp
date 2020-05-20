94231ad793f43850487f71e5436c3d9665ba8800
Shulman Egor <egorshulman@icloud.com>
2020-05-20 20:48:02 +0300

    fix

--- samples/sample_table.cpp
+++ samples/sample_table.cpp
@@ -54,3 +54,73 @@ int main()
 
     system("pause");
 }
+
+//Test SortTable
+/*int main()
+{
+    setlocale(LC_ALL, "rus");
+
+
+    try
+    {
+        Table<int, int> table(15000000);
+
+        auto& ti1 = typeid(table);
+        cout << ti1.name();
+
+        cout << endl;
+
+        for (int i = 15000000; i > 0; i--)
+        {
+            table.Add(100 + i, i * 10);
+        }
+        cout << endl;
+
+        unsigned int start_time = clock();
+        SortTable<int, int> table3(table);
+        unsigned int end_time = clock();
+        unsigned int search_time = end_time - start_time;
+
+        auto& ti2 = typeid(table3);
+        cout << ti2.name() << endl;
+
+        cout << "Время работы своей сортировки:: " << search_time << endl;
+
+        cout << endl;
+
+        start_time = clock();
+        sort(table.begin(), table.end(), [](Record<int, int>a, Record<int, int>b) ->bool {return a.key < b.key; });
+        end_time = clock();
+        search_time = end_time - start_time;
+
+        auto& ti3 = typeid(table);
+        cout << ti3.name() << endl;
+
+        cout << "Время работы std::sort: " << search_time << endl;
+        cout << endl;
+
+        cout << "Добавление данных по ключу 99 << endl;
+
+        cout << endl;
+
+        table3.Add(99, -99);
+
+        cout << "Поиск по ключу 99: " << table3.Find(99) << endl;
+
+        cout << endl;
+
+        cout << "Удаление данных по ключу 99:" << endl;
+        table3.Delete(99);
+        cout << endl;
+
+        cout << "Поиск по ключу 99: ";
+        cout << table3.Find(99) << endl;
+    }
+    catch (exception& e)
+    {
+        cerr << "Error: " << e.what() << endl;
+    };
+
+    system("pause");
+
+}*/
